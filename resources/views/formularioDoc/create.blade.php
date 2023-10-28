83f68fc4565b579067030b782a0176fe4f7c5775
Lazaro Francisco Juarez <lazarojuarez.pa@gmail.com>
2023-10-28 09:38:04 -0600

    Avance archivos

--- resources/views/formularioDoc/create.blade.php
+++ resources/views/formularioDoc/create.blade.php
@@ -17,29 +17,43 @@
                 <div class="card-header">{{ __('Ingresa tus documentos') }}</div>
 
                 <div class="card-body">
-                    <form >
-                        @csrf
+
 
                         <div class="row mb-3">
-                            <label for="acto_recepcional" class="col-md-4 col-form-label text-md-end">{{ __('Acto recepcional') }}</label>
-
+                            <label for="acto_recepcional" class="col-md-4 col-form-label text-md-end">{{ __('Solicitud de Acto recepcional') }}</label>
                             <div class="col-md-6">
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Acto recepcional</button>
+                                <button type="button" class="btn btn-primary" >Solicitud de Acto recepcional</button>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="acto_recepcional" class="col-md-4 col-form-label text-md-end">{{ __('Constancia de no inconveniencia') }}</label>
+                            <div class="col-md-6">
+                                <button type="button" class="btn btn-primary" >Constancia de no inconveniencia</button>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="acto_recepcional" class="col-md-4 col-form-label text-md-end">{{ __('Solicitud de Liberción de Proyecto') }}</label>
+                            <div class="col-md-6">
+                                <button type="button" class="btn btn-primary" >Solicitud de Liberción de Proyecto</button>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="acto_recepcional" class="col-md-4 col-form-label text-md-end">{{ __('AnteProyecto') }}</label>
+                            <div class="col-md-6">
+                                <button type="button" class="btn btn-primary" >AnteProyecto</button>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="acto_recepcional" class="col-md-4 col-form-label text-md-end">{{ __('Registro de Proyecto') }}</label>
+                            <div class="col-md-6">
+                                <button type="button" class="btn btn-primary" >Registro de Proyecto</button>
                             </div>
                         </div>
 
-                        <div class="row mb-0" style="text-align: center">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Guardar') }}
-                                </button>
 
-                                <a href="{{url()->previous()}}" class="btn btn-danger">
-                                    {{ __('Cancelar') }}
-                                </a>
-                            </div>
+
                         </div>
-                    </form>
+
                 </div>
             </div>
         </div>
@@ -47,24 +61,6 @@
 </div>
 
 
-{{-- Modal --}}
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="staticBackdropLabel">Título del modal</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-          <button type="button" class="btn btn-primary">Comprendido</button>
-        </div>
-      </div>
-    </div>
-  </div>
 @endsection
 
 
