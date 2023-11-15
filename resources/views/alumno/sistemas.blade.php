672fed6d251cc284665ca9c9ec251cfb00f0ff92
Lazaro Francisco Juarez <lazarojuarez.pa@gmail.com>
2023-11-15 08:15:29 -0600

    agregando el campo periodo a las tablas

--- resources/views/alumno/sistemas.blade.php
+++ resources/views/alumno/sistemas.blade.php
@@ -44,6 +44,7 @@
                             <th>Carrera</th>
                             <th>No. Control</th>
                             <th>Año</th>
+                            <th>Periodo</th>
                             <th>Telefono</th>
                             <th>Correo</th>
                             <th>Opciones</th>
@@ -61,6 +62,7 @@
                                     <td>{{ $value->carrera}}</td>
                                     <td>{{ $value->no_control}}</td>
                                     <td>{{ $value->anio}}</td>
+                                    <td>{{ $value->periodo}}</td>
                                     <td>{{ $value->telefono}}</td>
                                     <td>{{ $value->email }}</td>
                                     <td>
